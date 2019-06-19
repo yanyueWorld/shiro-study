d5a945234d7d6aec86a5694906aa891bd9bdbe35
yanyueWorld <1361557116@qq.com>
2019-06-19 17:01:37 +0800

    commond update

--- src/main/java/com/study/shiro/service/CustomRealmService.java
+++ src/main/java/com/study/shiro/service/CustomRealmService.java
@@ -1,7 +1,5 @@
 package com.study.shiro.service;
 
-import com.study.shiro.pojo.Role;
-
 
 import java.util.Set;
 
@@ -12,8 +10,36 @@ import java.util.Set;
  * @version Shiro v1.0
  */
 public interface CustomRealmService {
+
+    /**
+     * 通过用户名获得密码.
+     *
+     * @param username 用户名
+     * @return String 密码
+     */
     String getPasswordByUsername(String username);
+
+    /**
+     * 通过用户名获得角色.
+     *
+     * @param username 用户名
+     * @return Set<String> 角色集合
+     */
     Set<String> getRoleNameByUser(String username);
+
+    /**
+     * 通过用户名获得权限
+     *
+     * @param username 用户名
+     * @return Set<String> 权限集合
+     */
     Set<String> getPermissionsByUser(String username);
+
+    /**
+     * 通过用户名获取加密盐值
+     *
+     * @param username 用户名
+     * @return String 盐值
+     */
     String getSaltByUsername(String username);
 }
